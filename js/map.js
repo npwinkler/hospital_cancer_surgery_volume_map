214ff01f9bbf5ce972d077385486bca694204c6b
Nolan Winkler <npwinkler.work@gmail.com>
2020-02-04 19:55:49 -0600

    Create prototype of interactive map
    Took Derek Eder's working demo and changed the .csv to our CA hospitals and changed the text around the site.

---
+++ js/map.js
@@ -0,0 +1,74 @@
+$(window).resize(function () {
+  var h = $(window).height(),
+    offsetTop = 125; // Calculate the top offset
+
+  $('#mapCanvas').css('height', (h - offsetTop));
+}).resize();
+
+$(function() {
+
+  SearchableMapLib.initialize({
+    filePath: 'data/2019-12-17__ca_2017_without_nci.csv?1',
+    fileType: 'csv',
+    recordName: 'hospital',
+    recordNamePlural: 'hospitals',
+    map_centroid: [35.79268162, -119.627248],
+    defaultZoom:  6,
+    defaultRadius: 8050,
+    debug: true,
+  });
+
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('search-address'));
+  var modalURL;
+
+  $('#btnSearch').click(function(){
+    // Temporary fix for map load issue: set show map as default.
+    if ($('#mapCanvas').is(":visible")){
+      SearchableMapLib.doSearch();
+    }
+    else {
+      $('#btnViewMode').html("<i class='fa fa-list'></i> List view");
+      $('#mapCanvas').show();
+      $('#listCanvas').hide();
+      SearchableMapLib.doSearch();
+    }
+  });
+
+  $(':checkbox').click(function(){
+    SearchableMapLib.doSearch();
+  });
+
+  $(':radio').click(function(){
+    SearchableMapLib.doSearch();
+  });
+
+  $('#btnViewMode').click(function(){
+    if ($('#mapCanvas').is(":visible")){
+      $('#btnViewMode').html("<i class='fa fa-map-marker'></i> Map view");
+      $('#listCanvas').show();
+      $('#mapCanvas').hide();
+    }
+    else {
+      $('#btnViewMode').html("<i class='fa fa-list'></i> List view");
+      $('#listCanvas').hide();
+      $('#mapCanvas').show();
+    }
+  });
+
+  $("#search-address, #search-name").keydown(function(e){
+      var key =  e.keyCode ? e.keyCode : e.which;
+      if(key == 13) {
+          $('#btnSearch').click();
+          return false;
+      }
+  });
+
+  $(".close-btn").on('click', function() {
+    $.address.parameter('modal_id', null)
+  });
+
+});
+
+function formatAddress(prop) {
+    return prop["Street1"] + " " + prop["Street2"] + " " + prop["City"] + " " + prop["State"];
+}
